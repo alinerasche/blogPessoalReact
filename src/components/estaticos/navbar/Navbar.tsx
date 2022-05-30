8b3417f518486ceaf40264054073cd91ebafe1f5
Aline <aline.rasche25@gmail.com>
2022-05-30 18:45:51 -0300

    React Router

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography,Box } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core'
+import { Link } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import './Navbar.css'
@@ -9,42 +10,45 @@ function NavBar() {
         <>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer"}} >
-                    <Typography variant="h5" color="inherit">
-                        BlogPessoal
-                    </Typography>
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer"}}>
-                        <Typography variant="h6" color="inherit">
-                            Home
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
                         </Box>
-                    <Box mx={1} style={{ cursor: "pointer"}}>
-                         <Typography variant="h6" color="inherit">
-                             Postagens
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Postagens
+                            </Typography>
                         </Box>
-                    <Box mx={1} style={{ cursor: "pointer"}}>
-                    <Typography variant="h6" color="inherit">
-                            Temas
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Temas
+                            </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer"}}>
-                    <Typography variant="h6" color="inherit">
-                            Cadastrar Tema
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Cadastrar Tema
+                            </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer"}}>
-                    <Typography variant="h6" color="inherit">
-                            Logout
-                        </Typography>
-                        </Box>
-                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Link>
 
-                        </Toolbar>
-                        </AppBar>                    
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import { Box, Button, Grid, TextField, Typography } from '@material-ui/core'
-
-import "./Home.css"
+import React from "react";
+import { Typography, Grid, Button } from "@material-ui/core";
+import "./Home.css";
+import { Box } from "@mui/material";
 
 function Home() {
   return (
-    <Grid container className="background">
-      <Grid item xs={12}>
-
-        <Box display="flex" justifyContent="center" alignItems="center" height="80vh">
-          <Box className="card" width={340} height="50vh" borderRadius={5} 
-                marginTop={12} display="flex" justifyContent="center" alignItems="center">
-            <Box>
-              <Typography className='form-title' variant="h4" align="center">
-                Login
-              </Typography>
-
-              <form className='form'>
-                <Box marginY={4}>
-                  <TextField className='form-input' id="standard-basic" type="email" label="Email" required />
-                </Box>
-
-                <Box marginY={4}>
-                  <TextField  className='form-input' id="standard-basic" type="password" label="Senha" required />
-                </Box>
-
-                <Button variant="contained" color="secondary" className="botao">
-                  Login
-                </Button>
-              </form>
-
-            </Box>
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        className="caixa"
+      >
+        <Grid alignItems="center" item xs={6}>
+          <Box paddingX={20}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="titulo"
+            >
+              Seja bem vindo(a)!
+            </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              color="textPrimary"
+              component="h5"
+              align="center"
+              className="titulo"
+            >
+              expresse aqui os seus pensamentos e opiniões!
+            </Typography>
           </Box>
-        </Box>
-
+          <Box display="flex" justifyContent="center">
+            <Box marginRight={1}></Box>
+            <Button variant="outlined" className="botao">
+              Ver Postagens
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://i.imgur.com/H88yIo2.png"
+            alt=""
+            width="500px"
+            height="500px"
+          />
+        </Grid>
+        <Grid xs={12} className="postagens"></Grid>
       </Grid>
-    </Grid>
-  )
+    </>
+  );
 }
 
-export default Home
+export default Home;
